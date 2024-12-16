5de5d905c34efc1e596d8d71b56f2dd7535b4fac
s.tsuji <s.tsuji@fenrir.co.jp>
2024-12-16 12:27:16 +0900

    add: APPに描画するコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ParallaxProvider } from "react-scroll-parallax";
 import { Top } from "./Page/Top";
+// import { Page } from "./Page/Second";
+// import { Progress } from "./components/progress";
 
 function App() {
   return (
     <>
       <ParallaxProvider>
         <Top />
+        {/* <Page /> */}
+        {/* <Progress /> */}
       </ParallaxProvider>
     </>
   );
